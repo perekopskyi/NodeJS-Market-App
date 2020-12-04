6a32cf5e690fd709b19c076559e59d4f1bba2e44
Yevhenii Perekopskyi <yevheniiperekopskyi@gmail.com>
2020-12-04 22:23:56 +0200

    Added MVP App

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
+const cardRouter = require('./routes/card')
+
+const app = express()
+
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs',
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/card', cardRouter)
+
+const PORT = process.env.PORT || 3003
+app.listen(PORT, () => {
+  console.log(`Server is runing on http://localhost:${PORT}`)
+})
---
+++ routes/add.js
@@ -0,0 +1,20 @@
+const { Router } = require('express')
+const router = Router()
+const Course = require('../models/course')
+
+router.get('/', (req, res, next) => {
+  res.render('add', {
+    title: 'Add course',
+    isAdd: true,
+  })
+})
+
+router.post('/', async (req, res) => {
+  const { title, price, img } = req.body
+  const course = new Course(title, price, img)
+  await course.save()
+
+  res.redirect('/courses')
+})
+
+module.exports = router
